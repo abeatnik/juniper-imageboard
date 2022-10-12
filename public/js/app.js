cb9b88952d3f2d2f079eb5b2f3dba47b8f052f88
Tabea Schneider <abeat@posteo.net>
2022-10-12 18:42:57 +0200

    set up image board project

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-import * as Vue from './vue.js';
+import * as Vue from "./vue.js";
+
+Vue.createApp({
+    data() {
+        return {
+            images: "",
+        };
+    },
+    mounted() {
+        function fetchImages() {
+            const images = [];
+            for (let i = 0; i <= 12; i++) {
+                images.push(fetch("https://picsum.photos/200"));
+            }
+            return Promise.all(images);
+        }
+        fetchImages().then((data) => {
+            this.images = data.map((data) => data.url);
+        });
+    },
+}).mount("#main");
+
+//data defines a function that returns an object in which we can assign the variable that will be used in the app
